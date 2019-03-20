98f70897e6bf45601abc695c0e0221a11d70a9a9
cmercier2 <clementjrmercier@gmail.com>
2019-03-20 09:47:19 +0100

    servlet test web ui

--- src/com/excilys/utils/LoggerConfigurator.java
+++ src/com/excilys/utils/LoggerConfigurator.java
@@ -4,16 +4,21 @@ import org.apache.log4j.Logger;
 import org.apache.log4j.PropertyConfigurator;
 
 public class LoggerConfigurator {
-	
+
 	private static void addConfiguration() {
 		PropertyConfigurator.configure("./ressources/log4j.properties");
 	}
 
+	/**
+	 * 
+	 * @param classname
+	 * @return
+	 */
 	public static Logger configureLogger(Class<?> classname) {
 		Logger logger;
 		logger = Logger.getLogger(classname);
 		addConfiguration();
 		return logger;
 	}
-	
+
 }
