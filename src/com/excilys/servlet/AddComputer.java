a7fb607ca4cc3e49ef9486a47a0530ff0bced6b4
cmercier2 <clementjrmercier@gmail.com>
2019-03-28 16:47:02 +0100

    commit

--- src/com/excilys/servlet/AddComputer.java
+++ src/com/excilys/servlet/AddComputer.java
@@ -3,8 +3,6 @@ package com.excilys.servlet;
 import java.io.IOException;
 import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.Enumeration;
-
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
--- src/com/excilys/utils/ArgumentHandler.java
+++ src/com/excilys/utils/ArgumentHandler.java
@@ -2,7 +2,6 @@ package com.excilys.utils;
 
 import java.sql.Date;
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
 import java.util.Optional;
